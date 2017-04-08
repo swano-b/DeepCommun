8c2ac95699961aca5db7f321a131ef3299f9ae03
Baptiste <swano@swano-lab.net>
2017-04-08 18:21:59 +0200

    Fixes

--- Le-Deepcommun/Dossier libre/La désencyclopédie/Nazi - Désencyclopédie_fichiers/facebook_client_xfbml_js.js
+++
@@ -1,4 +0,0 @@
-
-mw.hook('wikipage.content').add(function($content){'use strict';if((xfbmlTagsOnPage())){$.loadFacebookSDK().done(renderXFBMLTags);}
-function xfbmlTagsOnPage(){var numOfXFBMLTags=$content.find('[data-type="xfbml-tag"]').length;return numOfXFBMLTags>0;}
-function renderXFBMLTags(){FB.XFBML.parse($content[0]);}});;
